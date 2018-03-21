9063855ca88d812f5fe799a1efcd093366897c6a
galr <galr@jfrog.com>
2018-03-21 23:17:37 +0200

    Added ex3

--- src/test/groovy/test.groovy
+++ src/test/groovy/test.groovy
@@ -1,19 +1,23 @@
+import groovy.json.*
 class test {
     static void main(String[] args) {
-//        "#####-Ex-1-#####"
-        String a = "Macabi Zona Macabi Zona Macabi Zona Macabi Zona"
-        println "The number of Zona is: ${a.findAll("Zona").size()}"
-        println "Lower case + without white spaces: ${a.toLowerCase().replaceAll(" ","")}"
-        println a.replaceAll("Macabi","Hapoel").replaceAll("Zona", "Alufa")
-        println "The number of a is: ${a.findAll("a").size()}"
+////        "#####-Ex-1-#####"
+//        String a = "Macabi Zona Macabi Zona Macabi Zona Macabi Zona"
+//        println "The number of Zona is: ${a.findAll("Zona").size()}"
+//        println "Lower case + without white spaces: ${a.toLowerCase().replaceAll(" ", "")}"
+//        println a.replaceAll("Macabi", "Hapoel").replaceAll("Zona", "Alufa")
+//        println "The number of a is: ${a.findAll("a").size()}"
+//
+////        "#####-Ex-2-#####"
+//        ArrayList<String> b = ["Gal", "Daniel", "Moshe", "Haim"]
+//        b.each { name ->
+//            def time = new Date()
+//            println "In the name ${name} there are ${name.size()} letters and the time is ${time.format("HH:mm:ss")}"
+//        }
 
-
-//        "#####-Ex-2-#####"
-        ArrayList<String> b = ["Gal","Daniel","Moshe","Haim"]
-        b.each {name ->
-            def time = new Date()
-            println "In the name ${name} there are ${name.size()} letters and the time is ${time.format("HH:mm:ss")}"
-        }
+        def jsonSlurper = new JsonSlurper()
+        def object = jsonSlurper.parseText('{ "myList": [4, 8, 15, 16, 23, 42] }')
+        println object.myList
 
     }
 }
